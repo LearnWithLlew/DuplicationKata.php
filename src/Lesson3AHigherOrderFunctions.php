1bd83946d009955f09c3dacab5ae14a2479fd381
Ed Walker <ed.walker@sincrodigital.com>
2021-02-10 20:59:04 -0800

    Fix formatting in Lesson3A & add missing type hints in src/.
    Also add strict_types to turn on strict type enforcement.

--- src/Lesson3AHigherOrderFunctions.php
+++ src/Lesson3AHigherOrderFunctions.php
@@ -1,45 +1,45 @@
 <?php
-
+declare(strict_types=1);
 
 namespace LearnWithLlew;
 
 
 class Lesson3AHigherOrderFunctions extends Song
 {
-    public function singCheers()
-  {
-    $number = 2;
-    $this->sing($number . "! ");
-    $number = $number + 2;
-    $this->sing($number . "! ");
-    $number = $number + 2;
-    $this->sing($number . "! ");
-    $number = $number + 2;
-    $this->sing($number . "! ");
-    $this->sing("Who do we appreciate?");
-    $number = 17;
-    $this->sing($number . "! ");
-    $number = $this->getNextPrime($number);
-    $this->sing($number . "! ");
-    $number = $this->getNextPrime($number);
-    $this->sing($number . "! ");
-    $number = $this->getNextPrime($number);
-    $this->sing($number . "! ");
-    $this->sing("These are the primes, that we find fine!");
-  }
-private function getNextPrime($number)
-  {
-      switch ($number)
-      {
-          case 13 :
-              return 17;
-          case 17 :
-              return 19;
-          case 19 :
-              return 23;
-          case 23 :
-              return 29;
-      }
-      return 0;
-  }
+    public function singCheers(): void
+    {
+        $number = 2;
+        $this->sing($number . "! ");
+        $number = $number + 2;
+        $this->sing($number . "! ");
+        $number = $number + 2;
+        $this->sing($number . "! ");
+        $number = $number + 2;
+        $this->sing($number . "! ");
+        $this->sing("Who do we appreciate?");
+        $number = 17;
+        $this->sing($number . "! ");
+        $number = $this->getNextPrime($number);
+        $this->sing($number . "! ");
+        $number = $this->getNextPrime($number);
+        $this->sing($number . "! ");
+        $number = $this->getNextPrime($number);
+        $this->sing($number . "! ");
+        $this->sing("These are the primes, that we find fine!");
+    }
+
+    private function getNextPrime(int $number): int
+    {
+        switch ($number) {
+            case 13 :
+                return 17;
+            case 17 :
+                return 19;
+            case 19 :
+                return 23;
+            case 23 :
+                return 29;
+        }
+        return 0;
+    }
 }
