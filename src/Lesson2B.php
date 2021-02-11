e6b2135334d6aa155c5766154ead2995a0e8dc86
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-02-11 02:14:02 +0000

    2B or not...

--- src/Lesson2B.php
+++ src/Lesson2B.php
@@ -11,25 +11,25 @@ class Lesson2B extends Song
         switch ($style) {
             case 1 :
                 foreach ($names as $name) {
-                    if (str_starts_with($name, "L")) {
-                        $this->sing("Hip Hip Horray! For " + $name);
+                    if (substr( $name, 0, strlen("L")) === "L") {
+                        $this->sing("Hip Hip Horray! For " . $name);
                     } else {
-                        $this->sing("Hello " + $name + ", it's nice to meet you.");
+                        $this->sing("Hello " . $name . ", it's nice to meet you.");
                     }
                 }
                 break;
             case 2 :
                 foreach ($names as $name) {
-                    if ($name . startsWith("am", 1)) {
-                        $this->sing("Say yeah! Say yo! Say " + $name);
+                    if (substr( $name, 1, strlen("am")) === "am") {
+                        $this->sing("Say yeah! Say yo! Say " . $name);
                     } else {
-                        $this->sing("Hello " + $name + ", it's nice to meet you.");
+                        $this->sing("Hello " . $name . ", it's nice to meet you.");
                     }
                 }
                 break;
             case 3 :
                 foreach ($names as $name) {
-                    $this->sing("Hello " + name + ", it's nice to meet you.");
+                    $this->sing("Hello " . $name . ", it's nice to meet you.");
                 }
                 break;
         }
--- tests/RegressionTest.php
+++ tests/RegressionTest.php
@@ -4,6 +4,7 @@ namespace LearningWithLlew\Tests;
 use ApprovalTests\Approvals;
 use LearnWithLlew\Lesson1AStraight;
 use LearnWithLlew\Lesson2AVariable;
+use LearnWithLlew\Lesson2B;
 use PHPUnit\Framework\TestCase;
 
 class RegressionTest extends TestCase
@@ -29,6 +30,16 @@ class RegressionTest extends TestCase
         // Act
         $lesson->singBottlesOfBeer();
         // Assert
+        Approvals::verifyString($lesson->song);
+    }
+    /** @test */
+    public function shouldTestLesson2B()
+    {
+        $lesson = new Lesson2B();
+        $names = ["Llewellyn", "Samatha", "Tomas", "Emilia"];
+        $lesson->singSong(1, $names);
+        $lesson->singSong(2, $names);
+        $lesson->singSong(3, $names);
         Approvals::verifyString($lesson->song);
     }
 }
